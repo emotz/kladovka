80eb90315631ee3487d25c0f780192bb30f54f48
ITarako <igtarako@gmail.com>
2017-04-03 16:08:33 +0700

    changed description of  main.test

--- main.test.js
+++ main.test.js
@@ -2,13 +2,13 @@ const klad = require('./main.js');
 const assert = require('assert');
 var kladovka = klad.kladovka;
 
-it('Сохранение предмета в кладовке под нужным id', function () {
+it('сохраняет предмет в кладовке под нужным id', function () {
     var item = { id: 33, dps: 100 };
     var newid = klad.placeInKladovka(item.id, item);
     assert(item.id == newid);
 });
 
-it('Изъятие предмета из кладовки', function () {
+it('получает предмет из кладовки', function () {
     var item = { id: 33, dps: 100 };
     klad.placeInKladovka(item.id, item);
     var newItem = klad.getFromKladovka(item.id);
@@ -16,21 +16,21 @@ it('Изъятие предмета из кладовки', function () {
     assert(item.dps === newItem.dps);
 });
 
-it('Удаление предмета из кладовки', function () {
+it('удаляет предмет из кладовки', function () {
     var item = { id: 33, dps: 100 };
     klad.placeInKladovka(item.id, item);
     klad.deleteFromKladovka(item.id);
     assert(klad.getFromKladovka(item.id) === undefined);
 });
 
-it('Сравнение двух различный предметов', function () {
+it('сравнивает 2 различный предмета', function () {
     var item1 = { id: 2, dps: 100, score: 300 };
     var item2 = { id: 1, dps: 100, score: 400 };
     var bestItem = klad.compareItems(item1, item2);
     assert(bestItem === item2);
 });
 
-it('Сравнение двух равных предметов', function () {
+it('сравнивает 2 одинаковых предмета', function () {
     var item1 = { id: 2, dps: 100, score: 300 };
     var item2 = { id: 1, dps: 100, score: 300 };
     var bestItem = klad.compareItems(item1, item2);

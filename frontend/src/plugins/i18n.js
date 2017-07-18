1ab023d1c13f859cf07616baed6b6fd464aac486
ITarako <igtarako@gmail.com>
2017-07-18 21:51:15 +0700

    closed #57

--- frontend/src/plugins/i18n.js
+++ frontend/src/plugins/i18n.js
@@ -1,5 +1,3 @@
-// временное решение, проблема будет решена в https://github.com/emotz/kladovka/issues/57
-import config from '../../../config/config.json';
 import en from '../i18ns/en.js';
 import ru from '../i18ns/ru.js';
 import Vue from 'vue';
@@ -7,6 +5,6 @@ import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
 
 export default new VueI18n({
-    locale: config.default_language,
+    locale: en,
     messages: { en, ru }
 });

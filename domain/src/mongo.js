983832aa21eca58cc8e74cb6aea46ebc79ff690b
ITarako <igtarako@gmail.com>
2017-07-31 15:42:39 +0700

    mongo without db argument

--- domain/src/mongo.js
+++ domain/src/mongo.js
@@ -3,31 +3,30 @@ const mongodb = require('mongodb');
 const mongo = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
 
+let db = {};
 /**
  * Открывает соединение с сервером MongoDB
  * @param {String} url - Адрес сервера MongoDB
  * @returns {Promise.<Object, Error>} БД
  */
 async function connect(url) {
-    return mongo.connect(url);
+    db = await mongo.connect(url);
 }
 
 /**
  * Закрывает соединение с сервером MongoDB
- * @param {Object} db - БД
  */
-function disconnect(db) {
+function disconnect() {
     db.close();
 }
 
 /**
  * Сохраняет объект в БД
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {Object} item - Этот объект будет сохранен в базу
  * @returns {Promise.<String, Error>} id добавленного объекта
  */
-async function addItem(db, coll, item) {
+async function addItem(coll, item) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     item = utility.clone(item);
@@ -37,12 +36,11 @@ async function addItem(db, coll, item) {
 
 /**
  * Получает из базы данных объект (не удалённый)
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {String} id - Идентификатор искомого объекта
  * @returns {Promise.<Object, Error>} Объект или null если такого объекта нет
  */
-async function getNotDeletedItemById(db, coll, id) {
+async function getNotDeletedItemById(coll, id) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let item = await collection.findOne({ _id: ObjectID(id), deleted: undefined });
@@ -51,12 +49,11 @@ async function getNotDeletedItemById(db, coll, id) {
 
 /**
  * Удаляет объект из БД
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {String} id - Идентификатор удаляемого объекта
  * @returns {Promise.<String, Error>} id удалённого объекта
  */
-async function deleteItemById(db, coll, id) {
+async function deleteItemById(coll, id) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.updateOne({ _id: ObjectID(id) }, { $set: { 'deleted': true } });
@@ -66,11 +63,10 @@ async function deleteItemById(db, coll, id) {
 
 /**
  * Удаляет ВСЕ объекты из БД
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @returns {Promise.<Number, Error>} Количество удалённых объектов
  */
-async function deleteAllItems(db, coll) {
+async function deleteAllItems(coll) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.updateMany({ deleted: undefined }, { $set: { deleted: true } });
@@ -79,11 +75,10 @@ async function deleteAllItems(db, coll) {
 
 /**
  * Получает массив объектов(не удалённых)
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @returns {Promise.<Array, Error>} Массив объектов
  */
-async function getNotDeletedItems(db, coll) {
+async function getNotDeletedItems(coll) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.find({ 'deleted': undefined }).toArray();
@@ -92,12 +87,11 @@ async function getNotDeletedItems(db, coll) {
 
 /**
  * Получает массив объектов(не удалённых) данного типа
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {Srting} type - Искомый тип
  * @returns {Promise.<Array, Error>} Массив объектов
  */
-async function getAllItemsByType(db, coll, type) {
+async function getAllItemsByType(coll, type) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.find({ deleted: undefined, type }).toArray();
@@ -106,12 +100,11 @@ async function getAllItemsByType(db, coll, type) {
 
 /**
  * Получает объект по заданному имени
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {Srting} name - Имя по которому проводится поиск
  * @returns {Promise.<Object, Error>} Объект или null если такого объекта нет
  */
-async function getNotDeletedItemByName(db, coll, name) {
+async function getNotDeletedItemByName(coll, name) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.findOne({ deleted: undefined, name });
@@ -120,13 +113,12 @@ async function getNotDeletedItemByName(db, coll, name) {
 
 /**
  * Заменяет объект по id
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @param {String} id - Идентификатор искомого объекта
  * @param {Object} item - Этот объект будет сохранен в базу
  * @returns {Promise.<Boolean, Error>} Истина если есть объект под данным id
  */
-async function replaceItemById(db, coll, id, item) {
+async function replaceItemById(coll, id, item) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.updateOne({ _id: ObjectID(id) }, { $set: item });
@@ -135,11 +127,10 @@ async function replaceItemById(db, coll, id, item) {
 
 /**
  * Очищает коллекцию
- * @param {Object} db - БД
  * @param {Srting} coll - Коллекция
  * @returns {Promise.<Number, Error>} Количество удалённых объектов
  */
-async function clearCollection(db, coll) {
+async function clearCollection(coll) {
     if (db === undefined) throw new Error('нет базы данных');
     let collection = db.collection(coll);
     let res = await collection.deleteMany({});

102d9cae5625a9363749fe4b8df1a889e7eb9958
ITarako <igtarako@gmail.com>
2017-08-09 13:51:40 +0700

    auth header into frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,11 @@ import 'bootstrap.css';
 import 'bootstrap';
 import 'nprogress.css';
 import Vue from 'vue';
-import VueResource from 'vue-resource';
 import VueResourceNProgress from 'vue-resource-nprogress';
 import VueStore from './plugins/vue-store';
+import './plugins/auth&http';
 import app from './components/kl-main.vue';
 
-Vue.use(VueResource);
 Vue.use(VueResourceNProgress);
 Vue.use(VueStore);
 new Vue(app).$mount('#app');
---
+++ frontend/src/plugins/auth&http.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.http.interceptors.push(function (request, next) {
+    let token = localStorage.getItem('token');
+    if (request.headers['Authorization'] === undefined && token) {
+        request.headers['Authorization'] = 'Bearer ' + token;
+        console.log('zaebca!');
+    }
+    next();
+});

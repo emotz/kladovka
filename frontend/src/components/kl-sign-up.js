473a7c89454a6e90d52518758a0f342ae1f7e4ad
ITarako <igtarako@gmail.com>
2017-08-11 03:26:23 +0700

    fixed chain of promises

--- frontend/src/components/kl-sign-up.js
+++ frontend/src/components/kl-sign-up.js
@@ -17,23 +17,24 @@ export default {
                 name: this.name,
                 password: this.password
             };
-            this.$http.post(API.USERS, user).then(response => {
-                this.$http.post(API.TOKENS, { email: this.email, password: this.password }).then(response => {
+            this.$http.post(API.USERS, user)
+                .then(() =>
+                    this.$http.post(API.TOKENS, { email: this.email, password: this.password }))
+                .then(response => {
                     localStorage.setItem('token', response.body.accessToken);
                     let user = _.upperFirst(response.body.user);
                     localStorage.setItem('user', user);
                     this.$emit('signIn', user);
+                }).catch(err => {
+                    if (err.status === 400 && err.body.code === 1) {
+                        let renderedErrors = renderValidationError(err.body.errors);
+                        renderedErrors.forEach(error => toastr.error(this.$t('errors.' + error.id, error.props)));
+                    } else if (err.status === 401 && err.body.code === 3) {
+                        let renderedErrors = renderValidationError(err.body.errors);
+                        renderedErrors.forEach(error => toastr.error(this.$t('errors.' + error.id, error.props)));
+                    } else
+                        toastr.error(this.$t('errors.default'));
                 });
-            }).catch(err => {
-                if (err.status === 400 && err.body.code === 1) {
-                    let renderedErrors = renderValidationError(err.body.errors);
-                    renderedErrors.forEach(error => toastr.error(this.$t('errors.' + error.id, error.props)));
-                } else if (err.status === 401 && err.body.code === 3) {
-                    let renderedErrors = renderValidationError(err.body.errors);
-                    renderedErrors.forEach(error => toastr.error(this.$t('errors.' + error.id, error.props)));
-                } else
-                    toastr.error(this.$t('errors.default'));
-            });
         }
     }
 };

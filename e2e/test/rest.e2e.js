3540a40a92e464aea806eb9c012e4f1af14ccca1
ITarako <igtarako@gmail.com>
2017-08-18 02:18:41 +0700

    some rest e2e tests

--- e2e/test/rest.e2e.js
+++ e2e/test/rest.e2e.js
@@ -2,150 +2,125 @@ const assert = require('assert');
 const expect = require('chai').expect;
 const axios = require('axios');
 
+const email = "testEmail";
+const name = "testName";
+const password = "testPassword";
+let token;
+
 describe('e2e test', function () {
 
-    describe('for user', function () {
+    before(async function () {
+        let res = await axios.post("http://localhost:8080/api/users", { email, name, password });
+        assert(res.status === 201);
+    });
 
-        it('should create new user', async function () {
-            let user = {
-                email: 'userEmail',
-                name: 'userName',
-                password: 'userPass',
-            };
-            let res = await axios.post('http://localhost:8080/api/users', user);
-            assert(res.status === 201);
-        });
+    it('should catch verification error: #emailAlreadyExists', async function () {
+        try {
+            await axios.post("http://localhost:8080/api/users", { email, name, password });
+        }
+        catch (e) {
+            assert(e.response.status === 400);
+            assert(e.response.data.code === 2);
+            expect(e.response.data.errors)
+                .to.have.deep.members([
+                    {
+                        id: "emailAlreadyExists",
+                        properties: ["email"]
+                    }
+                ]);
+            return;
+        }
+        assert(false);
+    });
 
-        describe('verification error', function () {
+    it('should get token', async function () {
+        let res = await axios.post("http://localhost:8080/api/tokens", { email, password });
+        assert(res.status === 200);
+        token = res.data.accessToken;
+    });
 
-            it('#emailAlreadyExists', async function () {
-                let user = {
-                    email: 'userEmail',
-                    name: 'userName',
-                    password: 'userPass',
-                };
-                let res = await axios.post('http://localhost:8080/api/users', user);
-                assert(res.status === 201);
-                try {
-                    await axios.post('http://localhost:8080/api/users', user);
-                } catch (e) {
-                    assert(e.response.data.code === 2);
-                    expect(e.response.data.errors)
-                        .to.have.deep.members([
-                            {
-                                id: "emailAlreadyExists",
-                                properties: ["email"]
-                            }
-                        ]);
-                    return;
-                }
-                assert(false);
+    it('should catch authentication error: #emailOrPasswordInvalid', async function () {
+        try {
+            await axios.post("http://localhost:8080/api/tokens", { email, password: "oops" });
+        } catch (e) {
+            assert(e.response.status === 400);
+            assert(e.response.data.code === 3);
+            expect(e.response.data.errors)
+                .to.have.deep.members([
+                    {
+                        id: "emailOrPasswordInvalid",
+                        properties: ["email", "password"]
+                    }
+                ]);
+            return;
+        }
+        assert(false);
+    });
+
+    it('should catch authorization error: #invalidToken', async function () {
+        let item = {
+            type: "axe",
+            minDmg: 1,
+            maxDmg: 2,
+            critDmg: 0
+        };
+        try {
+            await axios.post("http://localhost:8080/api/items", item, {
+                headers: { 'Authorization': 'Bearer ' + token + 'oops' }
             });
-        });
+        } catch (e) {
+            assert(e.response.status === 401);
+            assert(e.response.data.code === 4);
+            expect(e.response.data.errors)
+                .to.have.deep.members([
+                    {
+                        id: "invalidToken",
+                        properties: []
+                    }
+                ]);
+            return;
+        }
+        assert(false);
     });
 
 
-    describe('for item', function () {
-
-        describe('validation error', function () {
-
-            it('#notValidType: [type]', async function () {
-                let item = {
-                    type: 'enot',
-                    minDmg: 2,
-                    maxDmg: 3,
-                    critChance: 20,
-                    critDmg: 60
-                };
-                try {
-                    await axios.post('http://localhost:8080/api/items', item);
-                } catch (e) {
-                    assert(e.response.data.code === 1);
-                    expect(e.response.data.errors)
-                        .to.have.deep.members([
-                            {
-                                id: "notValidType",
-                                properties: ["type"]
-                            }
-                        ]);
-                    return;
-                }
-                assert(false);
+    it('should catch validation error: #mustBeNumber', async function () {
+        let item = {
+            type: "axe",
+            minDmg: 1,
+            maxDmg: 2,
+            critDmg: 0
+        };
+        try {
+            await axios.post("http://localhost:8080/api/items", item, {
+                headers: { 'Authorization': 'Bearer ' + token }
             });
-        });
+        } catch (e) {
+            assert(e.response.status === 400);
+            assert(e.response.data.code === 1);
+            expect(e.response.data.errors)
+                .to.have.deep.members([
+                    {
+                        id: "mustBeNumber",
+                        properties: ["critChance"]
+                    }
+                ]);
+            return;
+        }
+        assert(false);
     });
 
-    describe('for char', function () {
-
-        it('should create new char', async function () {
-            let char = {
-                _id: undefined,
-                atkSpd: 54,
-                dmg: 3,
-                critChance: 5,
-                critDmg: 6
-            };
-            let res = await axios.post('http://localhost:8080/api/chars', char);
-            assert(typeof (res.data.added_id) === 'string');
+    it('should add item', async function () {
+        let item = {
+            type: "axe",
+            minDmg: 1,
+            maxDmg: 2,
+            critDmg: 0,
+            critChance: 0
+        };
+        let res = await axios.post("http://localhost:8080/api/items", item, {
+            headers: { 'Authorization': 'Bearer ' + token }
         });
-
-        it('should create & update char', async function () {
-            let char = {
-                atkSpd: 54,
-                dmg: 3,
-                critChance: 5,
-                critDmg: 6
-            };
-            let res = await axios.post('http://localhost:8080/api/chars', char);
-            assert(typeof (res.data.added_id) === 'string');
-            let id = res.data.added_id;
-            res = await axios.put('http://localhost:8080/api/chars/' + id, char);
-            assert(res.status === 204);
-        });
-
-        it('should reset char', async function () {
-            let char = {
-                atkSpd: 54,
-                dmg: 3,
-                critChance: 5,
-                critDmg: 6
-            };
-            let res = await axios.post('http://localhost:8080/api/chars', char);
-            assert(typeof (res.data.added_id) === 'string');
-            let id = res.data.added_id;
-            char.atkSpd = 0;
-            char.dmg = 0;
-            char.critChance = 0;
-            char.critDmg = 0;
-            res = await axios.put('http://localhost:8080/api/chars/' + id, char);
-            assert(res.status === 204);
-        });
-
-        describe('validation error', function () {
-
-            it('#mustBeNumber: [atkSpd]', async function () {
-                let char = {
-                    _id: undefined,
-                    atkSpd: '54',
-                    dmg: 3,
-                    critChance: 5,
-                    critDmg: 6
-                };
-                try {
-                    await axios.post('http://localhost:8080/api/chars', char);
-                } catch (e) {
-                    assert(e.response.data.code === 1);
-                    expect(e.response.data.errors)
-                        .to.have.deep.members([
-                            {
-                                id: "mustBeNumber",
-                                properties: ["atkSpd"]
-                            }
-                        ]);
-                    return;
-                }
-                assert(false);
-            });
-        });
+        assert(res.status === 201);
     });
 });

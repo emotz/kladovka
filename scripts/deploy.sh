bd9c1433af28695977b1d0f89dcd649a74aed3bb
Igor Tarasenko <igtarako@gmail.com>
2017-09-11 00:51:35 +0700

    Deploy (#107)
    * added proc.env vars
    
    * travis -> heroku
    
    * fixed path to  script of deploy
    
    * test that travis can build only then 'Build pull request updates'
    
    * travis for master branch

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker-compose build prod
+docker tag kladovka/prod registry.heroku.com/kladovka/web
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" registry.heroku.com
+docker push registry.heroku.com/kladovka/web
